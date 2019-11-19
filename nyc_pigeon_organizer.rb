251f88e12dde0adf7592b9ae0b4900a4c11080f8
Renata Freitas <r.miriuk@gmail.com>
2019-11-19 16:40:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,15 +13,7 @@ def nyc_pigeon_organizer(data)
     newHash[e][:gender] = "female"
   #  newHash[e][:lives] = nil
   }
-  # data[:color].each { |pColor,pName| #pName is an array
-  #   newHash.each { |hName,hValue| #hName are hashes from the pigeons names, and Hvalue are their atributes
-  #     if pName.include?(hName) #working until here, should I loop inside it?
-  #       newHash[hName][:color] = pColor #My problem is, it only adds the last color
-  #       # hValue.each { |key,thisshit|
-  #       # thisshit.push(pColor)
-  #       # }
-  #     end
-  #   }
+
   data[:lives].each { |pColor,pName| #pName is an array
   newHash.each { |hName,hValue| #hName are hashes from the pigeons names, and Hvalue are their atributes
     if pName.include?(hName) #working until here, should I loop inside it?
